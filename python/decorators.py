c7ab21e54eb770bcb32fe238805b3d17e2518099
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-15 17:17:10 -0500

    more python decorators

--- python/decorators.py
+++ python/decorators.py
@@ -1,17 +1,66 @@
+# taken from http://www.artima.com/weblogs/viewpost.jsp?thread=240808
 
-class myDecorator(object):
+def try1():
 
-    def __init__(self, f):
-        print "inside myDecorator.__init__()"
-        f() # Prove that function definition has completed
+    class myDecorator(object):
 
-    def __call__(self):
-        print "inside myDecorator.__call__()"
+        def __init__(self, f):
+            print "inside myDecorator.__init__()"
+            f() # Prove that function definition has completed
 
-@myDecorator
-def aFunction():
-    print "inside aFunction()"
+        def __call__(self):
+            print "inside myDecorator.__call__()"
 
-print "Finished decorating aFunction()"
+    @myDecorator
+    def aFunction():
+        print "inside aFunction()"
 
-aFunction()
+    print "Finished decorating aFunction()"
+
+    aFunction()
+
+
+def try2():
+
+    class entryExit(object):
+
+        def __init__(self, f):
+            self.f = f
+
+        def __call__(self):
+            print "Entering", self.f.__name__
+            self.f()
+            print "Exited", self.f.__name__
+
+    @entryExit
+    def func1():
+        print "inside func1()"
+
+    @entryExit
+    def func2():
+        print "inside func2()"
+
+    func1()
+    func2()
+
+
+def try3():
+    def entryExit(f):
+        def new_f():
+            print "Entering", f.__name__
+            f()
+            print "Exited", f.__name__
+        return new_f
+
+    @entryExit
+    def func1():
+        print "inside func1()"
+
+    @entryExit
+    def func2():
+        print "inside func2()"
+
+    func1()
+    func2()
+    print func1.__name__
+    print func2.__name__
