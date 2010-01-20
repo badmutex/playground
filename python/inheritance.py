9f613898a527f90ec5fd6eb82ecfdd6955f46645
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-20 18:44:33 -0500

    [python] playing with multiple inheritance

--- python/inheritance.py
+++ python/inheritance.py
@@ -1,19 +1,28 @@
 
 class Base1:
+    def __init__(self):
+        self.var = 'b1!'
+
     def run(self):
-        return "Base1"
+        return "Base1 %s" % (self.var)
 
 class Base2:
+    def __init__(self):
+        self.var = 'b2!'
+
     def run(self):
-        return "Base2"
+        return "Base2 %s" % (self.var)
 
 class Child(Base1, Base2):
+    def __init__(self):
+        self.var = 'c!'
+
     def run(self):
         b1 = Base1.run(self)
         b2 = Base2.run(self)
-        return "%s\n%s\n%s" % (b1,b2,"Child")
+        return "%s\n%s\n%s %s" % (b1,b2,"Child", self.var)
 
 
-c = Child()
+zc = Child()
 print zc.run()
 
