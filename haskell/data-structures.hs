9a746ad5b73e55f3b4ed9f1a276dc9cdae3ea4ae
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-02-28 15:24:21 -0500

    [haskell] stack

--- haskell/data-structures.hs
+++ haskell/data-structures.hs
@@ -2,6 +2,7 @@ import System.Random
 
 data BinTree a = Leaf | Node a (BinTree a) (BinTree a) deriving Show
 
+
 inorder,preorder,postorder :: Show a => BinTree a -> [a]
 
 inorder   Leaf         = []
@@ -29,3 +30,33 @@ randomBT r l = do
   return t
 
 
+data Stack a = Tail | Front a (Stack a) deriving Show
+
+push :: a -> Stack a -> Stack a
+push a s = Front a s
+
+peak :: Stack a -> Maybe a
+peak Tail = Nothing
+peak (Front a _) = Just a
+
+pop :: Stack a -> Maybe (a, Stack a)
+pop Tail = Nothing
+pop (Front a s) = Just (a,s)
+
+mapStack :: (a -> b) -> Stack a -> Stack b
+mapStack _ Tail = Tail
+mapStack f (Front a s) = Front (f a) (mapStack f s)
+
+instance Functor Stack where fmap = mapStack
+
+fromListStack :: [a] -> Stack a
+fromListStack = foldr push Tail
+
+toListStack :: Stack a -> [a]
+toListStack Tail = []
+toListStack (Front a s) = a : toListStack s
+
+reverseStack :: Stack a -> Stack a
+reverseStack s = reverseStack' s Tail
+    where reverseStack' (Front a Tail) s = push a s
+          reverseStack' (Front a s) s'   = reverseStack' s (push a s')
