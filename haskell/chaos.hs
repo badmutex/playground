600cf89afe8f6c16cde4a2b9db3a4355464def32
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-03-20 14:17:31 -0400

    haskell: chaos game with gloss for opengl

---
+++ haskell/chaos.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Graphics.Gloss
+import System.Random
+import Control.Monad
+import Control.Monad.State
+import Data.List
+
+
+newPoint :: (Num a, Random a) => (a,a) -> IO (a,a)
+newPoint r = do
+  x <- randomRIO r
+  y <- randomRIO r
+  return (x,y)
+
+size = (1600,900)
+range = (0,900)
+
+type MyState = StateT (Point, [LabeledPoint]) IO
+
+data LabeledPoint = Label Int Point deriving (Eq, Show)
+
+die dim = randomRIO (1, dim)
+
+midPoint (x1,y1) (x2,y2) = ( (x1+x2) / 2,
+                             (y1+y2) / 2)
+
+choose i xs = find (\(Label j p) -> i == j) xs
+
+
+vertices dim range = do
+  ps <- replicateM dim $ newPoint range
+  return $ zipWith (\i p -> Label i p) [1..] ps
+
+nextPoint :: Int -> (Float, Float) -> MyState Picture
+nextPoint dim range = do
+  (prev, bounds) <- get
+  label <- liftIO $ die dim
+  let Just (Label _ vertex) = choose label bounds
+      next@(x,y) = midPoint prev vertex
+      state = (next, bounds)
+  put state
+  return $ Translate x y (Circle 0.4)
+
+
+loop :: Int -> Int -> Point -> MyState [Picture]
+loop 0 _ _ = return []
+loop n dim range = do
+  p <- nextPoint dim range
+  liftM (p :) (loop (n-1) dim range)
+
+
+chaos :: Int -> Int -> IO [Picture]
+chaos dim reps = do
+  seed <- newPoint range
+  bounds <- vertices dim range
+  evalStateT (loop reps dim range) (seed, bounds)
+
+
+
+main = do
+  pics <- chaos 4 100000
+  displayInWindow "hello world" size (0,0) white (Color black (Pictures pics))
