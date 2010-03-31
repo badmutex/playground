7b8170f06b2c029d0cba90ceb84be5a50d6291b7
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-03-31 12:23:17 -0400

    makeflow.hs: add a CmdBuilder monad using State and Monoid

--- haskell/makeflow.hs
+++ haskell/makeflow.hs
@@ -12,57 +12,74 @@ import Data.List
 import Data.Monoid
 
 
-data ShellType = Bash | Csh | Zsh
+data ShellType = Bash | Csh | Zsh deriving Show
 
 type Argument = String
 type OutputFile = String
 
-class HasOutputFile a where
-    output :: a -> String
 
-data Output a where
-    StdErr :: OutputFile -> Output ShellType
-    StdOut :: OutputFile -> Output ShellType
-    Separate :: Output ShellType -> Output ShellType ->  OutputFile  -> Output ShellType
-    Join :: OutputFile -> Output ShellType
+data Redirection where
+    CaptureStdErr :: OutputFile -> Redirection
+    CaptureStdOut :: OutputFile -> Redirection
+    Separate :: Redirection -> Redirection -> Redirection
+    Join :: OutputFile -> Redirection
+    Ignore :: Redirection
+    deriving Show
 
 data Program where
-    Executable :: FilePath  -> [Argument] -> Program
-    Pipe       :: Program   -> Program    -> Program
-    Redirect   :: Program    -> Output ShellType  -> Program
+    Executable :: FilePath  -> [Argument] -> Redirection -> Program
     Group      :: [Program] -> Program
     EmptyProgram :: Program
 
--- | TODO: finish
+instance Monoid Program where
+    mempty = EmptyProgram
+    mappend e1@(Executable _ _ _) e2@(Executable _ _ _) = Group [e1, e2]
+    mappend (Group g) e@(Executable _ _ _) = Group (e : g)
+    mappend e@(Executable _ _ _) g@(Group _) = mappend g e
+    mappend EmptyProgram EmptyProgram = EmptyProgram
+    mappend EmptyProgram p@(_) = p
+    mappend p@(_) EmptyProgram = p
+
 instance Show Program where
-    show (Executable path args) = printf "%s %s" path (intercalate " " args)
-    show (Group ps) = intercalate "\n" . map show $ ps
-    show EmptyProgram = ""
+    show (Executable path args redir) = printf "Executable %s %s %s" (show path) (show args) (show redir)
+    show (Group ps) = printf "Group %s" (show ps)
+    show EmptyProgram = "EmptyProgram"
 
 data Cmd = Cmd {
-      results :: [FilePath]
-    , depends :: [FilePath]
-    , program :: Program
-    }
+      cmd_results :: [FilePath]
+    , cmd_depends :: [FilePath]
+    , cmd_program :: Program
+    } deriving Show
 
 instance Monoid Cmd where
-    mempty = Cmd { results = [], depends = [], program = EmptyProgram }
-    mappend c1 c2 = Cmd { results = results c1 ++ results c2
-                        , depends = depends c1 ++ depends c2
-                        , program = Group [program c1, program c2]}
+    mempty = Cmd { cmd_results = [], cmd_depends = [], cmd_program = EmptyProgram }
+    mappend c1 c2 = Cmd { cmd_results = cmd_results c1 `mappend` cmd_results c2
+                        , cmd_depends = cmd_depends c1 `mappend` cmd_depends c2
+                        , cmd_program = cmd_program c1 `mappend` cmd_program c2   }
 
--- | TODO: finish
-build_cmd :: Cmd -> Program -> Cmd
-build_cmd cmd p@(Executable path _) = cmd {program = p}
-build_cmd cmd (Pipe p1 p2) = undefined
+newtype CmdBuilder a = CmdBuilder {
+      runCmdBuilder :: State Cmd a
+    } deriving (Monad, MonadState Cmd)
+
+buildCmd :: CmdBuilder a -> Cmd
+buildCmd = flip (execState . runCmdBuilder) mempty
+
+add_to_cmd :: Cmd -> (Cmd -> Cmd) -> Cmd
+add_to_cmd cmd mod = mappend cmd $ mod mempty
+
+modify_cmd val mod = get >>= put . flip add_to_cmd mod
+
+result :: FilePath -> CmdBuilder ()
+result path = modify_cmd path (\cmd -> cmd {cmd_results = [path]})
+
+depends :: FilePath -> CmdBuilder ()
+depends path = modify_cmd path (\cmd -> cmd {cmd_depends = [path]})
+
+program :: Program -> CmdBuilder ()
+program prog = modify_cmd prog (\cmd -> cmd {cmd_program = prog})
 
 
-instance Show Cmd where
-    show cmd = let fmt f = intercalate " " (f cmd)
-                   outs = fmt results
-                   deps = fmt depends
-                   prog = show $ program cmd
-               in printf "%s : %s\n\t%s" outs deps prog
+
 
 type Makeflow = [Cmd]
 
@@ -77,5 +94,11 @@ add_cmd cmd = modify (cmd :)
 clear :: Weaver ()
 clear = modify (const [])
 
-prog = Executable "/bin/ls" []
--- cmd = Cmd ['test.out
+prog = Executable "/bin/ls" [] Ignore
+
+test_cmdbuilder = buildCmd builder where
+    builder = do result "hello"
+                 result "world"
+                 depends "/bin/echo"
+                 program prog
+                 program $ Executable "/bin/who" [] (Join "who.out-err")
