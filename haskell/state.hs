8a4a1986017dc288a0a4e2e9027db2168551ff93
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-05-04 11:59:27 -0400

    haskell: State and Monad State and QuichCheck

--- haskell/state.hs
+++ haskell/state.hs
@@ -1,21 +1,65 @@
+{-# LANGUAGE FlexibleInstances, FunctionalDependencies, MultiParamTypeClasses #-}
+
+import Prelude hiding (Monad, return, (>>=), (>>), fail, mod)
+
+import Test.QuickCheck
+import Debug.Trace
 
 
 
-type State s a = s -> (s,a)
-
-ret :: a -> State s a
-ret x = \s -> (s,x)
-
-bind :: State s a -> (a -> State s b) -> State s b
-bind m f = \s -> let (s0, a) = m s
-                 in f a s0
-
-get :: State s s
-get = \s -> (s,s)
-
-put :: s -> State s ()
-put v = \s -> (v,())
+class Monad m where
+    return :: a -> m a
+    (>>=)  :: m a -> (a -> m b) -> m b
+    (>>)   :: m a -> m b -> m b
+    ma >> mb = ma >>= \_ -> mb
 
 
+newtype State s a = State { runState :: s -> (s, a) }
 
+
+instance Functor (State s) where
+    fmap f m = State $ \s -> let (s', a) = runState m s
+                             in (s', f a)
+
+
+instance Monad (State s) where
+    return v = State (\s -> (s, v))
+    m >>= f = State $ \s -> let (s', a) = runState m s
+                            in runState (f a) s'
+
+
+evalState :: State s a -> s -> a
+evalState m s = snd (runState m s)
+
+execState :: State s a -> s -> s
+execState m s = fst (runState m s)
+
+
+
+class Monad m => MonadState m s | m -> s where
+    get :: m s
+    put :: s -> m ()
+    mod :: (s -> s) -> m ()
+    mod f = get >>= (put . f)
+
+instance MonadState (State s) s where
+    get   = State $ \s -> (s, s)
+    put s = State $ \_ -> (s, ())
+
+
+
+type Test = State Int
+
+inc :: Test ()
+inc = mod (+1)
+
+inc' :: Test ()
+inc' = fmap (+1) get >>= return >>= put
+
+
+
+prop_test :: Int -> Bool
+prop_test s = let t = (==) (s+1) . flip execState s
+                  xs = map t [inc, inc']
+              in and xs
 
