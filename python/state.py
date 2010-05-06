aa8e3f86d863e6fb22a013b3bb6d6013ded19823
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-05-05 20:57:26 -0400

    python: state monad

--- python/state.py
+++ python/state.py
@@ -1,19 +1,24 @@
 
 
 
-def lift_state (v): return lambda s: (s, v)
+def returns(v):
+    return lambda s: (s,v)
 
-def bind_state (m, f):
-    def bind_run_state(s):
-        s0, a = m (s)
-        return f (a, s0)
-    return bind_run_state
+def bind(m, f):
+    def binder(s0):
+        s1, v = m(s0)
+        return f(v)(s1)
+
+def gets():
+    return lambda s: (s,s)
+
+def puts(s):
+    return lambda _: (s, None)
+
+def mods(f):
+    return lambda s: (f(s), None)
 
 
-def get_state (s): return (s,s)
 
-def put_state (v): lambda s: (v, None)
+incs = mods(lambda v: v+1)
 
-
-    
-    
