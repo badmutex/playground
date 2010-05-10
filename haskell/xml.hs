0b272b6b44aa47a7190eea7bf9cab2c6df244254
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-05-10 18:48:46 -0400

    xml: save

--- haskell/xml.hs
+++ haskell/xml.hs
@@ -2,17 +2,25 @@
 
 import Text.XML.HXT.Arrow
 
+import Text.ParserCombinators.Parsec
+
 import System.Environment
 import Data.Char
 import Debug.Trace as D
 
 
 
+data GProtein = Gq | Gs | Gi | G12 deriving (Eq, Show)
+type Specificity = Double
+
+newtype Result = Result [(GProtein, Specificity)]
+
+
 test doc out = runX $ readDocument [(a_parse_html, "1"), (a_trace, "0")] doc
                   >>> deep isText
                   >>> writeDocument [(a_indent, "1")] out
 
-test2 as doc = runX (rundoc as doc)
+test2 as doc = runX (rundoc as doc) >>= return . extract
 
 don't_keep = ["\t","\n","\n\t"]
 
@@ -30,6 +38,29 @@ application al src
 
 getNodes = deep (hasAttr "color" `when` isText)
 processNodes = deep getText
+
+specificity :: Parser String
+specificity = do
+  d <- many digit
+  char '.'
+  d' <- many digit
+  return $ d ++ "." ++ d'
+
+gprotein :: String -> Parser String
+gprotein s = do
+  string $ s ++ " - \t"
+  specificity
+
+gq = gprotein "Gq/11"
+gi = gprotein "Gi/o"
+gs = gprotein "Gs"
+g12 = gprotein "G12/13"
+
+
+
+
+
+extract = concat
 
 
 
