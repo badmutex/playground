877c74a6eb66c454af9b263b0d0105822089c2cd
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-05-10 23:27:57 -0400

    updated Sim to incorporate new Vector

--- haskell/sim/Math/Sim.hs
+++ haskell/sim/Math/Sim.hs
@@ -2,18 +2,22 @@ module Math.Sim where
 
 import Prelude as P
 
-import Math.Vector as V
+import Math.Vector
 
-type Position = Tagged D3 (Vector Double)
-type Velocity = Tagged D3 (Vector Double)
+import Data.Vector as V
+
+
+type Position = Vec D3 Vector Double
+type Velocity = Vec D3 Vector Double
+type Force    = Vec D3 Vector Double
 type Mass     = Double
-type Force    = Double
 type Index    = Int
 
 data Particle = P { pos  :: !Position
                   , vel  :: !Velocity
                   , mass :: !Mass
                   }
+              deriving Show
 
 type Particles = Vector Particle
 
@@ -22,6 +26,11 @@ data SimState = S { particles :: Particles }
 
 
 
--- -- force_between :: Particle -> Particle -> Force
-force_between p1 p2 = let top = (mass p1 * mass p2) *> pos p1 <-> pos p2
-                      in top
+force_between :: Particle -> Particle -> Force
+force_between p1 p2 = let top = (mass p1 * mass p2) *> (pos p1 <-> pos p2)
+                          bot = norm (pos p1 <-> pos p2) ^3
+                      in top </ bot
+
+
+p1 = P (vector' [1..3]) (vector' [0,0,0]) 9
+p2 = P (vector' [4..6]) (vector' [0,0,0]) 9
