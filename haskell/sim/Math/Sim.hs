5aae1474c3a64a001b447c538ac581523005e1a2
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-05-10 22:47:26 -0400

    cleanup

--- haskell/sim/Math/Sim.hs
+++ haskell/sim/Math/Sim.hs
@@ -1,9 +1,8 @@
+module Math.Sim where
 
-
-import Data.Vector as V
 import Prelude as P
 
-import Math.Vector
+import Math.Vector as V
 
 type Position = Tagged D3 (Vector Double)
 type Velocity = Tagged D3 (Vector Double)
@@ -22,29 +21,7 @@ type Particles = Vector Particle
 data SimState = S { particles :: Particles }
 
 
-class Vec a n where
-    (<->) :: a -> a -> a
-    (*>)  :: n -> a -> a
 
-
-instance Vec Position Double where
-    (<->) = vecOp (-)
-    (*>)  = scalarMul
-
-
-
-vecOp :: (a -> b -> c) -> Tagged t (Vector a) -> Tagged t (Vector b) -> Tagged t (Vector c)
-vecOp op a b = let (x,y) = (untag a, untag b)
-               in T $ V.zipWith op x y
-
--- scalarMul :: Num n => n -> Tagged t (Vector n) -> Tagged t (Vector n)
--- scalarMul a v = vecOp (*) (T $ V.replicate 3 a) v
-
-
--- (<->) = vecOp (-)
--- (*>)  = scalarMul
-
--- force_between :: Particle -> Particle -> Force
+-- -- force_between :: Particle -> Particle -> Force
 force_between p1 p2 = let top = (mass p1 * mass p2) *> pos p1 <-> pos p2
-
                       in top
