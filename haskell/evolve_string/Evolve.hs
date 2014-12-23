693d9e7fc3f61254e8257c62fe41d14125eec0be
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-12-23 11:21:27 -0500

    get seeding RNG to work

--- haskell/evolve_string/Evolve.hs
+++ haskell/evolve_string/Evolve.hs
@@ -1,61 +1,65 @@
 
+import Data.IORef
 import Data.Ord
 import Data.List
-import Data.Random
 import Control.Monad
+import Control.Monad.IO.Class
 import Control.Applicative
 import Text.EditDistance
+import Data.Random
+import Data.Random.Lift
 import System.Random
 
 
 
 chars = ['a'..'z'] ++ ['A'..'Z'] ++ " !'"
 
-newChar :: IO Char
-newChar = go
-    where go = do i <- randomRIO (0, length chars - 1)
-                  return $ chars !! i
+newChar :: RVarT IO Char
+newChar = lift $ randomElement chars
 
+sampleRange :: (Enum a, Random a) => (a, a) -> RVarT IO a
+sampleRange (low,high) = lift $ randomElement [low..high]
 
 newtype Solution = Solution { solstr :: String }
     deriving (Show)
 
 type Target = Solution
 
-mutate :: Solution -> IO Solution
+mutate :: Solution -> RVarT IO Solution
 mutate (Solution s) = do
-  i <- randomRIO (0, length s - 1)
+  i <- sampleRange (0, length s - 1)
   c <- newChar
   let (l,r) = splitAt i s
       new_s = l ++ c : drop 1 r
-  -- putStrLn $ "Mut@" ++ show i ++ ": " ++ s ++ " -> " ++ new_s
+  -- liftIO $ putStrLn $ "Mut@" ++ show i ++ ": " ++ s ++ " -> " ++ new_s
   return $ Solution new_s
   
 
-cross :: Solution -> Solution -> IO Solution
+cross :: Solution -> Solution -> RVarT IO Solution
 cross (Solution s1) (Solution s2)
-    | length s1 == length s2 = do i <- randomRIO (0, length s1-1)
+    | length s1 == length s2 = do i <- sampleRange (0, length s1-1)
                                   let new_s = take i s1 ++ drop i s2
                                   -- putStrLn $ "Cross: " ++ s1 ++ " X " ++ s2 ++ " -> " ++ new_s
                                   return $ Solution new_s
     | otherwise              = fail $ "Size mismatch: '" ++ s1 ++ "', '" ++ s2 ++ "'"
 
 
+
 fitness :: Target -> Solution -> Double
 fitness (Solution s) (Solution t) = fromIntegral $ levenshteinDistance defaultEditCosts s t
 
-randomSolution :: Int -> IO Solution
+randomSolution :: Int -> RVarT IO Solution
 randomSolution len = Solution <$> mapM (const newChar) [1..len] 
 
-initialize :: Int -> Int -> IO [Solution]
+
+initialize :: Int -> Int -> RVarT IO [Solution]
 initialize popsize strsize
                     | odd popsize = initialize (popsize +1) strsize
                     | otherwise   = mapM (const (randomSolution strsize)) [1..popsize]
 
-
-step :: Target -> [Solution] -> IO [Solution]
+step :: Target -> [Solution] -> RVarT IO [Solution]
 step target pop = do
-  pop' <- sample $ shuffle pop
+  pop' <- lift $ shuffle pop
   let len = length pop
       mid = length pop `div` 2
       (l, r) = (take mid pop', drop mid pop')
@@ -63,14 +67,15 @@ step target pop = do
   let newpop = take len $ sortBy (comparing (fitness target)) $ children ++ pop
   return newpop
 
-
-runGA :: Int -> Target -> [Solution] -> IO Solution
+runGA :: Int -> Target -> [Solution] -> RVarT IO Solution
 runGA gens target pop = go 0 pop
     where go gen pop = let fit = minimum $ map (fitness target) pop
-                       in do putStrLn $ "[" ++ show gen ++ "]: " ++ show fit ++ " @ " ++ solstr (head pop)
+                       in do liftIO $ putStrLn $ "[" ++ show gen ++ "]: " ++ show fit ++ " @ " ++ solstr (head pop)
                              if or [fit < 1, gen >= gens ]
                                 then return $ head pop
                                 else step target pop >>= go (gen+1)
+
+
 
 
 
@@ -78,12 +83,14 @@ data Parameters = Parms { popSize :: Int
                         , maxGens :: Int
                         , seed    :: Int }
 
-defParms = Parms { popSize = 50
+defParms = Parms { popSize = 200
                  , maxGens = 9000
                  , seed    = 0 }
 
 run :: String -> IO Solution
 run target = let prms = defParms
              in do
-               setStdGen $ mkStdGen (seed prms)
-               initialize (popSize prms) (length target) >>= runGA (maxGens prms) (Solution target)
+               stdgen <- newIORef $ mkStdGen $ seed prms
+               flip runRVarT stdgen $ do
+                  initialize (popSize prms) (length target)
+                  >>= runGA (maxGens prms) (Solution target)
