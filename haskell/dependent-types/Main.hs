8aba855aa9645eb66d5963ae9e6729e105079cda
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-07-06 16:09:00 -0400

    haskell/dependent types: get "zeros" to work

--- haskell/dependent-types/Main.hs
+++ haskell/dependent-types/Main.hs
@@ -1,17 +1,27 @@
-{-# LANGUAGE DataKinds      #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 
+
+import           Data.Proxy
+import           Data.Reflection
 import           Data.Singletons
+import           Data.Singletons.Prelude
+import           Data.Singletons.TypeLits
 import           GHC.TypeLits
 
-newtype V (x :: Nat) a =
-  V [a]
-  deriving (Eq, Show)
+-- a type-indexed list
+newtype List (x :: Nat) a = List [a] deriving (Eq, Show)
 
 
-v0 :: V 0 a
-v0 = V []
+v0 :: List 0 a
+v0 = List []
 
-v1 :: a -> V 1 a
-v1 a = V [a]
+v1 :: a -> List 1 a
+v1 a = List [a]
 
+zeros :: (Num a, KnownNat n) => List n a
+zeros = withSing go
+  where
+    go :: (Num a, KnownNat n) => Sing n -> List n a
+    go s = let size = fromIntegral $ fromSing s
+           in List $ take size $ repeat 0
