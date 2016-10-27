b6ce0e2c2a2d59c3004df63897aa051b526772e2
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-10-27 16:58:28 -0400

    elm: elm-book/Elm Architecture/Messages with increment state

---
+++ elm/www.elm-tutorial.org/ElmArchitecture/Message2.elm
@@ -0,0 +1,40 @@
+module Main exposing (..)
+import  Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Html.App
+
+type alias Model
+    = Int
+
+init : (Model, Cmd Msg)
+init = (0, Cmd.none)
+
+type Msg
+    = Incr Int
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick (Incr 1) ] [ text "+" ]
+        , text (toString model)
+        ]
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Incr n ->
+            (model + n, Cmd.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
