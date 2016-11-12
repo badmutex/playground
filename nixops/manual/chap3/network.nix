0421fda1f8e478f6c5f7efee780b251c7c5dd8e3
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-11-12 13:46:03 -0500

    nixops: chap3: single machine

---
+++ nixops/manual/chap3/network.nix
@@ -0,0 +1,11 @@
+{
+  network.description = "Web server";
+
+  webserver =
+    { config, pkgs, ... }:
+    { services.httpd.enable = true;
+      services.httpd.adminAddr = "alice@example.org";
+      services.httpd.documentRoot = "${pkgs.valgrind.doc}/share/doc/valgrind/html";
+      networking.firewall.allowedTCPPorts = [ 80 ];
+    };
+}
---
+++ nixops/manual/chap3/vbox.nix
@@ -0,0 +1,8 @@
+{
+  webserver =
+    { config, pkgs, ... }:
+    { deployment.targetEnv = "virtualbox";
+      deployment.virtualbox.memorySize = 1024; # megabytes
+      deployment.virtualbox.headless = true;
+    };
+}
